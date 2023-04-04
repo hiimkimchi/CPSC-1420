4ce66ee2cc21c86310671a5598891dd99f0b1412
Bryan Kim <103244689+hiimkimchi@users.noreply.github.com>
2023-04-04 16:42:51 -0700

    Uploaded lab1
    Hello world

---
+++ lab1/lab1.cpp
@@ -0,0 +1,15 @@
+//Bryan Kim
+//lab1.cpp
+//9/26/2022
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << endl;
+  cout << "Name: Bryan Kim\n" << endl;
+  cout << "Pronouns: He/Him\n" << endl;
+  cout << "Favorite Movie: Parasite\n" << endl;
+  return 0;
+}
