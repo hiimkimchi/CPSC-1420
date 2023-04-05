d8926020ee30205f0ba51fb6eb29665f5db92ef6
Bryan Kim <103244689+hiimkimchi@users.noreply.github.com>
2023-04-04 18:22:36 -0700

    Updated lab1
    added descriptions

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,14 +1,16 @@
 //Bryan Kim
 //lab1.cpp
+//This lab introduces us to C++ by printing "Hello World"
 
 #include <iostream>
 using namespace std;
 
+//desc: print hello world
+//pre : none
+//post: none
 int main()
 {
   cout << endl;
-  cout << "Name: Bryan Kim\n" << endl;
-  cout << "Pronouns: He/Him\n" << endl;
-  cout << "Favorite Movie: Parasite\n" << endl;
+  cout << "Hello World" << endl;
   return 0;
 }
